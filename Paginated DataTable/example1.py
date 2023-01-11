845a3a4d7176efbb79eaa4d702913fc0a8f8fcfb
ndonkoHenri <robotcoder4@protonmail.com>
2023-01-11 13:13:13 +0100

    Update example1.py

--- Paginated DataTable/example1.py
+++ Paginated DataTable/example1.py
@@ -1,32 +1,56 @@
 import flet as ft
-from simpledt import CSVDataTable       # pip install simpledatatable
 from paginated_dt import PaginatedDataTable
 
 
 def main(page: ft.Page):
-    page.title = "Paginated DataTable Ex1"
+    page.title = "Paginated DataTable Example 1"
     page.theme_mode = "light"
     page.scroll = ft.ScrollMode.AUTO
     page.window_always_on_top = True
 
-    csv = CSVDataTable("Test Data/day.csv")
-    pdt = PaginatedDataTable(datatable=csv.datatable, table_title="Customer Invoices", rows_per_page=5)
+    # copied from the flet.DataTable docs: https://flet.dev/docs/controls/datatable#examples
+    dt = ft.DataTable(
+            width=700,
+            bgcolor="yellow",
+            border=ft.border.all(2, "red"),
+            border_radius=10,
+            vertical_lines=ft.border.BorderSide(3, "blue"),
+            horizontal_lines=ft.border.BorderSide(1, "green"),
+            sort_column_index=0,
+            sort_ascending=True,
+            heading_row_color=ft.colors.BLACK12,
+            heading_row_height=100,
+            data_row_color={ft.MaterialState.HOVERED: "0x30FF0000"},
+            show_checkbox_column=True,
+            divider_thickness=0,
+            column_spacing=200,
+            columns=[
+                ft.DataColumn(
+                    ft.Text("Column 1"),
+                    on_sort=lambda e: print(f"{e.column_index}, {e.ascending}"),
+                ),
+                ft.DataColumn(
+                    ft.Text("Column 2"),
+                    tooltip="This is a second column",
+                    numeric=True,
+                    on_sort=lambda e: print(f"{e.column_index}, {e.ascending}"),
+                ),
+            ],
+            rows=[
+                ft.DataRow(
+                    [ft.DataCell(ft.Text("A")), ft.DataCell(ft.Text("1"))],
+                    selected=True,
+                    on_select_changed=lambda e: print(f"row select changed: {e.data}"),
+                ),
+                ft.DataRow([ft.DataCell(ft.Text("B")), ft.DataCell(ft.Text("2"))]),
+            ],
+        )
 
-    # change the borders of the data table
-    pdt.datatable.border = ft.border.all(4, ft.colors.BLUE_ACCENT_700)
-
-    # modify the rows in the data table
-    for i in pdt.datarows:
-        rownum = i.cells[0].content.value
-        if int(rownum) % 2 == 0:
-            i.color = ft.colors.GREEN
-
-    # modify the columns in the data table
-    for i in pdt.datacolumns:
-        i.label = ft.Row([i.label, ft.Icon(ft.icons.AC_UNIT)])
+    pdt = PaginatedDataTable(datatable=dt, table_title="Sample Data", rows_per_page=5)
 
     page.add(
-        pdt
+        pdt,
+        dt
     )
 
 
