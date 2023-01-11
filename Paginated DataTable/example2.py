e2ead212f83b82dfc3f27e986f15a6dcfcc6c3ac
ndonkoHenri <robotcoder4@protonmail.com>
2023-01-11 13:13:08 +0100

    Update example2.py

--- Paginated DataTable/example2.py
+++ Paginated DataTable/example2.py
@@ -3,47 +3,44 @@ from paginated_dt import PaginatedDataTable
 
 
 def main(page: ft.Page):
-    page.title = "Paginated DataTable Ex1"
+    page.title = "Paginated DataTable Example 2"
     page.theme_mode = "light"
     page.scroll = ft.ScrollMode.AUTO
     page.window_always_on_top = True
 
+    # copied from the flet.DataTable docs: https://flet.dev/docs/controls/datatable#examples
     dt = ft.DataTable(
-            width=700,
-            bgcolor="yellow",
-            border=ft.border.all(2, "red"),
-            border_radius=10,
-            vertical_lines=ft.border.BorderSide(3, "blue"),
-            horizontal_lines=ft.border.BorderSide(1, "green"),
-            sort_column_index=0,
-            sort_ascending=True,
-            heading_row_color=ft.colors.BLACK12,
-            heading_row_height=100,
-            data_row_color={ft.MaterialState.HOVERED: "0x30FF0000"},
-            show_checkbox_column=True,
-            divider_thickness=0,
-            column_spacing=200,
             columns=[
-                ft.DataColumn(
-                    ft.Text("Column 1"),
-                    on_sort=lambda e: print(f"{e.column_index}, {e.ascending}"),
-                ),
-                ft.DataColumn(
-                    ft.Text("Column 2"),
-                    tooltip="This is a second column",
-                    numeric=True,
-                    on_sort=lambda e: print(f"{e.column_index}, {e.ascending}"),
-                ),
+                ft.DataColumn(ft.Text("First name")),
+                ft.DataColumn(ft.Text("Last name")),
+                ft.DataColumn(ft.Text("Age"), numeric=True),
             ],
             rows=[
                 ft.DataRow(
-                    [ft.DataCell(ft.Text("A")), ft.DataCell(ft.Text("1"))],
-                    selected=True,
-                    on_select_changed=lambda e: print(f"row select changed: {e.data}"),
+                    cells=[
+                        ft.DataCell(ft.Text("John")),
+                        ft.DataCell(ft.Text("Smith")),
+                        ft.DataCell(ft.Text("43")),
+                    ],
                 ),
-                ft.DataRow([ft.DataCell(ft.Text("B")), ft.DataCell(ft.Text("2"))]),
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text("Jack")),
+                        ft.DataCell(ft.Text("Brown")),
+                        ft.DataCell(ft.Text("19")),
+                    ],
+                ),
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text("Alice")),
+                        ft.DataCell(ft.Text("Wong")),
+                        ft.DataCell(ft.Text("25")),
+                    ],
+                ),
             ],
         )
+
+    # create an instance of the PaginatedDataTable
     pdt = PaginatedDataTable(datatable=dt, table_title="Sample Data", rows_per_page=5)
 
     page.add(
