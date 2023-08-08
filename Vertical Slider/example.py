6b05732c8638d0998a0bec02914a262fb778d4ee
ndonkoHenri <robotcoder4@protonmail.com>
2023-08-08 22:06:00 +0200

    Update example.py

--- Vertical Slider/example.py
+++ Vertical Slider/example.py
@@ -1,4 +1,3 @@
-# this example tries to mimic this flutter example: https://flutterawesome.com/flutter-vertical-slider-widget-to-customize-the-appearance-of-the-slider/
 import flet as ft
 from vertical_slider import VerticalSlider
 
@@ -10,27 +9,33 @@ def main(page: ft.Page):
     page.window_height = 400
     page.theme = ft.Theme(visual_density=ft.ThemeVisualDensity.ADAPTIVEPLATFORMDENSITY)
 
+    vs_1 = VerticalSlider(
+        value=50,
+        label="%{value}",
+        slider_width=80,
+        text_style=ft.TextStyle(color=ft.colors.WHITE, size=24),
+        active_track_color=ft.colors.PRIMARY,
+        inactive_track_color=ft.colors.PRIMARY_CONTAINER,
+        on_change=lambda val: print(f"slider_1_value = {val}")
+    )
+
+    vs_2 = VerticalSlider(
+        value=40,
+        label="%{value}",
+        slider_width=80,
+        round_to=0,
+        text_style=ft.TextStyle(color=ft.colors.WHITE, size=24),
+        active_track_color=ft.colors.AMBER,
+        inactive_track_color=ft.colors.AMBER_100,
+        on_change=lambda val: print(f"slider_2_value = {val}")
+    )
+
     page.add(
         ft.Container(
             content=ft.Row(
                 controls=[
-                    VerticalSlider(
-                        value=50,
-                        label="%{value}",
-                        slider_width=80,
-                        text_style=ft.TextStyle(color=ft.colors.WHITE, size=24),
-                        active_track_color=ft.colors.PRIMARY,
-                        inactive_track_color=ft.colors.PRIMARY_CONTAINER
-                    ),
-                    VerticalSlider(
-                        value=40,
-                        label="%{value}",
-                        slider_width=80,
-                        round_to=0,
-                        text_style=ft.TextStyle(color=ft.colors.WHITE, size=24),
-                        active_track_color=ft.colors.AMBER,
-                        inactive_track_color=ft.colors.AMBER_100
-                    )
+                    vs_1,
+                    vs_2
                 ],
                 alignment=ft.MainAxisAlignment.CENTER,
                 spacing=35
